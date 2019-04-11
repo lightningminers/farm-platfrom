fd6c7dcebd0e270eddf0a256c73cb5326de99327
icepy <xiangwenwe@foxmail.com>
2019-04-11 22:33:06 +0800

    remove lib

--- packages/farm-translation/lib/mapping.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mapping = {
-    "zh_CN": "zh",
-    "en": "en",
-    "zh_TW": "cht"
-};
-exports.default = mapping;
